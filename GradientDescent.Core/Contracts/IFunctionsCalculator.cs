4ac8ef4665db0b668a155a42ff824819c5e52776
Roman Bodnar <rbod@LT-218338>
2017-10-12 13:37:48 +0300

    Implemented FunctionsCalculator

---
+++ GradientDescent.Core/Contracts/IFunctionsCalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace GradientDescent.Core.Contracts
+{
+    public interface IFunctionsCalculator
+    {
+        double Hypothesis(double[] features, double[] theta);
+
+        double CostFuction(double[] theta);
+    }
+}
---
+++ GradientDescent.Core/Services/FunctionsCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using GradientDescent.Core.Contracts;
+using GradientDescent.Data.DataSource.Contracts;
+
+namespace GradientDescent.Core.Services
+{
+    public class FunctionsCalculator : IFunctionsCalculator
+    {
+        private readonly IValuesNormalizer valuesNormalizer;
+        private readonly IDataSourceSupplier dataSupplier;
+
+        public FunctionsCalculator(IValuesNormalizer valuesNormalizer, IDataSourceSupplier dataSupplier)
+        {
+            this.valuesNormalizer = valuesNormalizer;
+            this.dataSupplier = dataSupplier;
+        }
+
+        public double Hypothesis(double[] features, double[] theta)
+        {
+            double hypothesis = features[0] * theta[0] + features[1] * theta[1] + features[2] * theta[2] +
+                                features[3] * theta[3] + features[4] * theta[4];
+            return hypothesis;
+        }
+
+        public double CostFuction(double[] theta)
+        {
+            throw new NotImplementedException();
+            //var houses = dataSupplier.GetData().ToList();
+            //var numberOfTrainingExamples = houses.Count;
+            //double result = 0.0;
+            //for (int i = 1; i <= numberOfTrainingExamples; i++)
+            //{
+            //    var features = GetFeaturesVector(houses[i]);
+            //    result += Math.Pow(Hypothesis(features, theta) - houses[i].Price, 2);
+            //}
+            //return 1 * result / (2 * numberOfTrainingExamples);
+        }
+    }
+}
