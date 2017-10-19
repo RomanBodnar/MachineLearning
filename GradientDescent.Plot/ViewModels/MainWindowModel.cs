15068ebb82235a919f01717e157c17d21903f424
Roman Bodnar <rbod@LT-218338>
2017-10-19 13:43:55 +0300

    OxyPlot set up

---
+++ GradientDescent.Plot/ViewModels/MainWindowModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GradientDescent.Plot.Annotations;
+using OxyPlot;
+
+namespace GradientDescent.Plot.ViewModels
+{
+    public class MainWindowModel
+    {
+        public MainWindowModel()
+        {
+            this.Title = "Example 2";
+            this.Points = new List<DataPoint>
+            {
+                new DataPoint(0, 4),
+                new DataPoint(10, 13),
+                new DataPoint(20, 15),
+                new DataPoint(30, 16),
+                new DataPoint(40, 12),
+                new DataPoint(50, 12)
+            };
+        }
+
+        public string Title { get; private set; }
+
+        public IList<DataPoint> Points { get; private set; }
+    }
+}
